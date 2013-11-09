d7ed3e607bd48cea8a6c720e66a4dfae4366678e
Daniel <daniel@seeclickfix.com>
2013-11-09 14:59:32 -0500

    Whitespace removal

--- src/Torann/Registry/RegistryServiceProvider.php
+++ src/Torann/Registry/RegistryServiceProvider.php
@@ -4,44 +4,44 @@ use Illuminate\Support\ServiceProvider;
 
 class RegistryServiceProvider extends ServiceProvider {
 
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->package('torann/registry');
-    }
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('torann/registry');
+	}
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app['registry'] = $this->app->share(function($app)
-        {
-            return new Registry($app);
-        });
-    }
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['registry'] = $this->app->share(function($app)
+		{
+			return new Registry($app);
+		});
+	}
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return array('registry');
-    }
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('registry');
+	}
 
 }
