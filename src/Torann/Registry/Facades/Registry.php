76fc2d9206043830189436f0ce1283bab8a1716c
Alexandr Shevchenko <ls.shov@gmail.com>
2018-01-16 15:29:41 +0300

    Added hints for phpStorm auto-complete

--- src/Torann/Registry/Facades/Registry.php
+++ src/Torann/Registry/Facades/Registry.php
@@ -1,7 +1,22 @@
 <?php namespace Torann\Registry\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Illuminate\Database\DatabaseManager;
+use Torann\Registry\Cache;
 
+/**
+ * The facade for @see \Torann\Registry\Registry
+ * @property $cache
+ *
+ * @method __construct(DatabaseManager $database, Cache $cache, $config = array())
+ * @method get($key, $default = null)
+ * @method set($key, $value)
+ * @method overwrite($key, $value)
+ * @method store(array $values)
+ * @method forget($key)
+ * @method flush()
+ * @method all($default = array())
+ */
 class Registry extends Facade {
 
 	/**
